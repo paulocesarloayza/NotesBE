b8ba7a0060934f0d63fba94f07ef6af50fac1de8
Paulo Cesar Loayza <pcloayza@gmail.com>
2019-05-20 22:41:31 -0400

    con modelo

---
+++ NotesBE/NotesBE/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace NotesBE.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<NotesBE.Models.Notes> Notes { get; set; }
+    }
+}
---
+++ NotesBE/NotesBE/Models/Notes.cs
@@ -0,0 +1,21 @@
+﻿namespace NotesBE.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Notes
+    {
+        [Key]
+        public int NotesId { get; set; }
+        public string Contents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Notes()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+
+    }
+}
